db661fb1fb28a01cded50a8e67a8673ad149d718
Tran Van Quyet <55977448+QuyetTV1991@users.noreply.github.com>
2023-11-03 02:33:29 +0000

    fix the year type

--- show_car/components/CustomFilter.tsx
+++ show_car/components/CustomFilter.tsx
@@ -1,16 +1,15 @@
-'use client'
+"use client";
 
-import { Fragment, useState } from 'react'
-import Image from 'next/image'
-import { Listbox, Transition } from '@headlessui/react'
-import { CustomFilterProps } from '@types'
+import { Fragment, useState } from "react";
+import Image from "next/image";
+import { Listbox, Transition } from "@headlessui/react";
+import { CustomFilterProps } from "@types";
 
-const CustomFilter = ({title, options, setFilter}:CustomFilterProps) => {
-
-  const [selected, setSelected] = useState(options[0])
+const CustomFilter = ({ title, options, setFilter }: CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0]);
 
   return (
-    <div className='w-fit'>
+    <div className="w-fit">
       <Listbox
         value={selected}
         onChange={(e) => {
@@ -18,28 +17,42 @@ const CustomFilter = ({title, options, setFilter}:CustomFilterProps) => {
           setFilter(e.value);
         }}
       >
-        <div className='relative w-fit z-10'>
+        <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
-            <span className='block truncate'>{selected.title}</span>
-            <Image 
+            <span className="block truncate">{selected.title}</span>
+            <Image
               src="/chevron-up-down.svg"
-              alt='chevron up down'
+              alt="chevron up down"
               width={20}
               height={20}
-              className='ml-4 object-contain'
+              className="ml-4 object-contain"
             />
           </Listbox.Button>
           <Transition
             as={Fragment}
-            leave='transition ease-in duration-100'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <Listbox.Options className='custom-filter__options'>
-              {options.map((option,index) => (
-                <Listbox.Option key={index} value={option} className={({active}) => `relative cursor-default select-none py-2 px-4 ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}>
-                  {( selected ) => (
-                    <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>{option.title}</span>
+            <Listbox.Options className="custom-filter__options">
+              {options.map((option, index) => (
+                <Listbox.Option
+                  key={index}
+                  value={option}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 px-4 ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
+                >
+                  {(selected) => (
+                    <span
+                      className={`block truncate ${
+                        selected ? "font-medium" : "font-normal"
+                      }`}
+                    >
+                      {option.title}
+                    </span>
                   )}
                 </Listbox.Option>
               ))}
@@ -48,7 +61,7 @@ const CustomFilter = ({title, options, setFilter}:CustomFilterProps) => {
         </div>
       </Listbox>
     </div>
-  )
-}
+  );
+};
 
-export default CustomFilter
+export default CustomFilter;
